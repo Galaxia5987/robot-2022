ba78eb0ad4478f839612a15b5a568f44971b35d2
Adam <adam.vichik@gmail.com>
2022-01-11 14:10:20 +0200

    Update ctre version

--- src/main/java/frc/robot/subsystems/example/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/example/ExampleSubsystem.java
@@ -4,14 +4,16 @@ package frc.robot.subsystems.example;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Ports;
 
 public class ExampleSubsystem extends SubsystemBase {
     private static ExampleSubsystem INSTANCE = null;
     // create fields here
-    private final TalonFX main = new TalonFX(Ports.ExampleSubsystem.MAIN);
-    private final TalonSRX aux = new TalonSRX(Ports.ExampleSubsystem.AUX);
+    private final WPI_TalonFX main = new WPI_TalonFX(Ports.ExampleSubsystem.MAIN);
+    private final WPI_TalonSRX aux = new WPI_TalonSRX(Ports.ExampleSubsystem.AUX);
 
     private ExampleSubsystem() {
         // motor and sensor inversions
@@ -42,7 +44,7 @@ public class ExampleSubsystem extends SubsystemBase {
      * @param power the output of the motor in percent [-1, 1].
      */
     public void setPower(double power) {
-        main.set(ControlMode.PercentOutput, power);
+        main.set(power);
     }
 
 }
