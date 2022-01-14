4fc4fde8bc4b000adaa8943edfd8c1cc1df9818b
eitan <eitan.vichik@gmail.com>
2022-01-14 14:26:53 +0200

    Move Ports from Code Drafts repo
    This also includes removing the example subsystem.

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,13 +1,11 @@
 package frc.robot;
 
 public final class Ports {
-    public static class ExampleSubsystem {
-        public static final int MAIN = 0;
-        public static final int AUX = 0;
-        public static final boolean IS_MAIN_INVERTED = false;
-        public static final boolean IS_AUX_INVERTED = false;
-        public static final boolean IS_MAIN_SENSOR_INVERTED = false;
-        public static final boolean IS_AUX_SENSOR_INVERTED = false;
+    public static class Shooter {
+        public static final int SOLENOID = 0;
+        public static final int MAIN_MOTOR = 0;
+        public static final boolean MAIN_INVERTED = false;
+        public static final boolean MAIN_SENSOR_PHASE = false;
     }
 
     public static class Controls {
