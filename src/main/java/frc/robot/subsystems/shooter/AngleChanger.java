cdf2a831cb0aabfd5f8b99ec47bbde446700ce85
eitan <eitan.vichik@gmail.com>
2022-01-14 14:28:46 +0200

    Move AngleChanger from Code Drafts repo

---
+++ src/main/java/frc/robot/subsystems/shooter/AngleChanger.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Ports.Shooter.SOLENOID;
+
+public class AngleChanger extends SubsystemBase {
+    private final Solenoid angle_changer = new Solenoid(PneumaticsModuleType.CTREPCM, SOLENOID);
+    private static final AngleChanger INSTANCE = new AngleChanger();
+
+    public AngleChanger() {
+    }
+
+    public void changeAngle(){
+        angle_changer.set(!angle_changer.get());
+    }
+
+    public boolean getActive(){
+        return angle_changer.get();
+    }
+
+    public static AngleChanger getINSTANCE(){
+        return INSTANCE;
+    }
+}
