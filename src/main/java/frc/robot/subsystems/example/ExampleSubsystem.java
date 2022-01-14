523aa76014d0baa9c9c3a49ad3494e1b347f3339
Adam <adam.vichik@gmail.com>
2022-01-14 10:21:54 +0200

    Delete imports

--- src/main/java/frc/robot/subsystems/example/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/example/ExampleSubsystem.java
@@ -1,13 +1,9 @@
 package frc.robot.subsystems.example;
 
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Ports;
 
