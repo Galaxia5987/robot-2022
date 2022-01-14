fd0e265f7a32f7b18596b1b57e1e0b239fe833d5
eitan <eitan.vichik@gmail.com>
2022-01-14 14:24:32 +0200

    Move Shooter from Code Drafts repo

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,5 +1,105 @@
 package frc.robot.subsystems.shooter;
 
-public class Shooter {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.LinearQuadraticRegulator;
+import edu.wpi.first.math.estimator.KalmanFilter;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.LinearSystemLoop;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.UnitModel;
+import frc.robot.utils.Utils;
 
+import static frc.robot.Constants.LOOP_PERIOD;
+import static frc.robot.Constants.Shooter.*;
+import static frc.robot.Ports.Shooter.*;
+
+public class Shooter extends SubsystemBase {
+    private static final Shooter INSTANCE = new Shooter();
+    private final UnitModel unitModel = new UnitModel(TICKS_PER_METER);
+    private final WPI_TalonFX main_motor = new WPI_TalonFX(MAIN_MOTOR);
+    private final LinearSystemLoop<N1, N1, N1> linearSystemLoop;
+    private final DCMotor motor = DCMotor.getFalcon500(1);
+
+    private Shooter() {
+        main_motor.setInverted(MAIN_INVERTED);
+        main_motor.setSensorPhase(MAIN_SENSOR_PHASE);
+//        linearSystemLoop = configStateSpaceKaKv();
+        linearSystemLoop = stateSpaceConfigInertia();
+    }
+
+    public static Shooter getINSTANCE() {
+        return INSTANCE;
+    }
+
+    private LinearSystemLoop<N1, N1, N1> configStateSpaceKaKv() {
+        LinearSystem<N1, N1, N1> flywheel_plant = new LinearSystem<>(
+                A_KaKv, B_KaKv, C_KaKv, D_KaKv);
+
+        LinearQuadraticRegulator<N1, N1, N1> quadraticRegulator = new LinearQuadraticRegulator<>(
+                flywheel_plant,
+                VecBuilder.fill(MODEL_TOLERANCE),
+                VecBuilder.fill(SENSOR_TOLERANCE),
+                LOOP_PERIOD);
+
+        KalmanFilter<N1, N1, N1> kalmanFilter = new KalmanFilter<>(
+                Nat.N1(), Nat.N1(),
+                flywheel_plant,
+                Matrix.mat(Nat.N1(), Nat.N1()).fill(MODEL_TOLERANCE),
+                Matrix.mat(Nat.N1(), Nat.N1()).fill(SENSOR_TOLERANCE),
+                LOOP_PERIOD);
+
+        return new LinearSystemLoop<>(
+                flywheel_plant,
+                quadraticRegulator,
+                kalmanFilter,
+                NOMINAL_VOLTAGE, LOOP_PERIOD);
+    }
+
+    private LinearSystemLoop<N1, N1, N1> stateSpaceConfigInertia() {
+        LinearSystem<N1, N1, N1> flywheel_plant = LinearSystemId.createFlywheelSystem(motor, J, GEAR_RATIO);
+
+        LinearQuadraticRegulator<N1, N1, N1> quadraticRegulator = new LinearQuadraticRegulator<>(
+                flywheel_plant,
+                VecBuilder.fill(MODEL_TOLERANCE),
+                VecBuilder.fill(SENSOR_TOLERANCE),
+                LOOP_PERIOD);
+
+        KalmanFilter<N1, N1, N1> kalmanFilter = new KalmanFilter<>(
+                Nat.N1(), Nat.N1(),
+                flywheel_plant,
+                Matrix.mat(Nat.N1(), Nat.N1()).fill(MODEL_TOLERANCE),
+                Matrix.mat(Nat.N1(), Nat.N1()).fill(SENSOR_TOLERANCE),
+                LOOP_PERIOD);
+
+        return new LinearSystemLoop<>(
+                flywheel_plant,
+                quadraticRegulator,
+                kalmanFilter,
+                NOMINAL_VOLTAGE, LOOP_PERIOD);
+    }
+
+    public double getVelocity() {
+        return unitModel.toUnits(main_motor.getSelectedSensorVelocity());
+    }
+
+    public void setVelocity(double velocity, double timeInterval) {
+        velocity = Utils.deadband(velocity, 0.1);
+
+        linearSystemLoop.setNextR(VecBuilder.fill(velocity));
+        linearSystemLoop.correct(VecBuilder.fill(getVelocity()));
+        linearSystemLoop.predict(timeInterval);
+
+        main_motor.set(ControlMode.PercentOutput, linearSystemLoop.getU(0) / NOMINAL_VOLTAGE);
+    }
+
+    public void terminate() {
+        main_motor.set(ControlMode.PercentOutput, 0);
+    }
 }
