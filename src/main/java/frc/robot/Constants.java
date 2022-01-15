dff1cfc1c9c9e6b8c55f8f4b200f4ab044078d00
eitan <eitan.vichik@gmail.com>
2022-01-15 16:08:19 +0200

    Constants documentation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,46 +8,43 @@ import edu.wpi.first.math.numbers.N1;
 
 public final class Constants {
     public static final double LOOP_PERIOD = 0.02; // loop period. [s]
-    public static final double g = 9.80665;
-    public static final double UPPER_TARGET_HEIGHT = 2.64;
-    public static final double PEBZNER_HEIGHT = 4.8;
+    public static final double g = 9.80665; // Gravity acceleration constant. [m/s^2]
+    public static final double UPPER_TARGET_HEIGHT = 2.64; // Height of upper target. [m]
+    public static final double PEBZNER_HEIGHT = 4.8; // Height of pebzner auditorium. [m]
     public static final int TALON_TIMEOUT = 10; // Waiting period for configurations [ms].
-    public static final double NOMINAL_VOLTAGE = 12; // [volts]
-    public static final boolean ENABLE_VOLTAGE_COMPENSATION = true;
-    public static final boolean ENABLE_CURRENT_LIMIT = true;
 
     public static class Shooter {
         public static final double BALL_RADIUS = 0.12; // radius of the ball. [m]
         public static final double BALL_WEIGHT = 0.27; // weight of the ball. [kg]
-        public static final double AREA_OF_ATTACK = Math.PI * Math.pow(BALL_RADIUS, 2);
-        public static final double Cd = 0.4;
-        public static final double AIR_DENSITY = 1.225;
+        public static final double AREA_OF_ATTACK = Math.PI * Math.pow(BALL_RADIUS, 2); // Area that air resistance hits the ball in. [m^2]
+        public static final double Cd = 0.4; // Drag coefficient of the ball.
+        public static final double AIR_DENSITY = 1.225; // Average density of air. [kg/m^3]
 
-        public static final double NOMINAL_VOLTAGE = 12.0;
-        public static final double WHEEL_RADIUS = 1;
-        public static final int TICKS_PER_REVOLUTION = 2048;
+        public static final double NOMINAL_VOLTAGE = 12.0; // Nominal voltage. [V]
+        public static final double WHEEL_RADIUS = 1; // Radius of the flywheel. [m]
+        public static final int TICKS_PER_REVOLUTION = 2048; // Ticks per revolution of the shooter motor. [tick/c]
         public static final double TICKS_PER_METER = Math.pow(WHEEL_RADIUS, 2) * Math.PI
-                / TICKS_PER_REVOLUTION; // ticks per meter of the wheel. [tick/m]
+                / TICKS_PER_REVOLUTION; // Ticks per meter of the shooter flywheel. [tick/m]
 
-        public static final double Ka = 1;
-        public static final double Kv = 1;
+        public static final double Ka = 1; // Acceleration state space coefficient (placeholder).
+        public static final double Kv = 1; // Velocity state space coefficient (placeholder).
         public static final Matrix<N1, N1> A_KaKv = new MatBuilder<>(Nat.N1(), Nat.N1()).fill(
-                -Kv / Ka);
+                -Kv / Ka); // Linear system A value for Ka and Kv state space.
         public static final Matrix<N1, N1> B_KaKv = new MatBuilder<>(Nat.N1(), Nat.N1()).fill(
-                1 / Ka);
+                1 / Ka); // Linear system B value for Ka and Kv state space.
         public static final Matrix<N1, N1> C_KaKv = new MatBuilder<>(Nat.N1(), Nat.N1()).fill(
-                1);
+                1); // Linear system C value for Ka and Kv state space.
         public static final Matrix<N1, N1> D_KaKv = new MatBuilder<>(Nat.N1(), Nat.N1()).fill(
-                0);
-        public static final double MODEL_TOLERANCE = 0.8;
-        public static final double SENSOR_TOLERANCE = 0.2;
+                0); // Linear system D value for Ka and Kv state space.
+        public static final double MODEL_TOLERANCE = 0.8; // Model tolerance for state space.
+        public static final double SENSOR_TOLERANCE = 0.2; // Sensor tolerance for state space.
 
-        public static final double J = 0.00032;
-        public static final double GEAR_RATIO = 1;
+        public static final double J = 0.00032; // Moment of inertia for state space. [kg*m^2]
+        public static final double GEAR_RATIO = 1; // Gear ratio for encoder (placeholder).
     }
 
     public static class AngleChanger {
-        public static final double ACTIVE_ANGLE = 55;
-        public static final double INACTIVE_ANGLE = 70;
+        public static final double ACTIVE_ANGLE = 55; // Angle of the shooter when the angle changer is active (placeholder). [deg]
+        public static final double INACTIVE_ANGLE = 70; // Angle of the shooter when the angle changer is inactive (placeholder). [deg]
     }
 }
