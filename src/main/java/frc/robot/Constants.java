4659f139218d0fe3f990762416f4f22e7d5eda19
Barel Shlidor <Barel@shlidor.org>
2022-01-20 14:43:02 +0200

    Create an example for the use webconstant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,13 @@
 package frc.robot;
 
 
+import frc.robot.valuetuner.WebConstant;
+
 public final class Constants {
 
     public static class ExampleSubsystem {
-        public static final double POWER = 0.5; // [%]
+        private static final String NAME = ExampleSubsystem.class.getName();
+        public static final WebConstant POWER = WebConstant.of(NAME, "power", 0.5); // [%]
     }
 
 }
