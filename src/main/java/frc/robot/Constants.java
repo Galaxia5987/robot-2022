735411104874684d81bda9ea9bd3250642527dd9
eitan <eitan.vichik@gmail.com>
2022-01-21 13:03:40 +0200

    Initialise constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,9 +11,9 @@ public final class Constants {
     }
 
     public static class Vision {
-        public static final double CAMERA_HEIGHT = 0; // [m]
-        public static final double TARGET_HEIGHT = 0; // [m]
-        public static final double CAMERA_PITCH = 0; // [rads]
+        public static final double CAMERA_HEIGHT = 0.767; // [m]
+        public static final double TARGET_HEIGHT = 2.64; // [m]
+        public static final double CAMERA_PITCH = 0.471239; // [rads]
 
         public static final Pose2d HUB_POSE = new Pose2d();
         public static final Transform2d CAMERA_TO_ROBOT = new Transform2d();
