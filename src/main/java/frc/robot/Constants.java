a62129fbb8bb527ed66b8e40ae149353b57d63a3
eitan <eitan.vichik@gmail.com>
2022-01-21 15:17:14 +0200

    Begin work on simulation
    There is an issue involving displaying the data.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,9 +2,14 @@ package frc.robot;
 
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import org.photonvision.SimVisionTarget;
 
 public final class Constants {
+    public static final double FIELD_WIDTH = 8.23; // Width of the field. [m]
+    public static final double FIELD_LENGTH = 16.46; // Length of the field. [m]
 
     public static class ExampleSubsystem {
         public static final double POWER = 0.5; // [%]
@@ -12,11 +17,21 @@ public final class Constants {
 
     public static class Vision {
         public static final double CAMERA_HEIGHT = 0.767; // [m]
-        public static final double TARGET_HEIGHT = 2.64; // [m]
-        public static final double CAMERA_PITCH = 0.471239; // [rads]
+        public static final double TARGET_HEIGHT_FROM_GROUND = 2.64; // [m]
+        public static final double CAMERA_PITCH = 0.471239; // Pitch of the vision. [rads]
+        public static final double DIAG_FOV = 75; // Diagonal FOV. [deg]
+        public static final double LED_RANGE = 6; // Visible range of LEDs. [m]
+        public static final int CAM_RESOLUTION_WIDTH = 640; // Width of camera resolution. [pixel]
+        public static final int CAM_RESOLUTION_HEIGHT = 480; // Height of camera resolution. [pixel]
+        public static final double MIN_TARGET_AREA = 10; // Minimal area of target. [pixel^2]
+        public static final double TARGET_WIDTH = 1.36; // Width of vision target strip. [m]
+        public static final double TARGET_HEIGHT = 0.05; // Height of the vision target strip. [m]
 
-        public static final Pose2d HUB_POSE = new Pose2d();
+        public static final Pose2d HUB_POSE = new Pose2d(
+                new Translation2d(FIELD_WIDTH / 2, FIELD_LENGTH / 2), new Rotation2d());
         public static final Transform2d CAMERA_TO_ROBOT = new Transform2d();
 
+        public static final SimVisionTarget HUB = new SimVisionTarget(
+                HUB_POSE, TARGET_HEIGHT_FROM_GROUND, TARGET_WIDTH, TARGET_HEIGHT);
     }
 }
