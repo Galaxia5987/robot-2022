43b24f07fc597f33675f1d3b17aaa64fc614b97e
eitan <eitan.vichik@gmail.com>
2022-01-21 11:41:25 +0200

    Initialise angles

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     public static final int TALON_TIMEOUT = 10; // Waiting period for configurations [ms].
 
     public static class Hood {
-        public static final double CLOSED_ANGLE = 55; // Angle for longer distance (placeholder). [deg]
-        public static final double OPEN_ANGLE = 70; // Angle for shorter distance (placeholder). [deg]
+        public static final double CLOSED_ANGLE = 54; // Angle for longer distance. [deg]
+        public static final double OPEN_ANGLE = 69; // Angle for shorter distance. [deg]
     }
 }
