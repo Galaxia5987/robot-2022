ae89b3ab70702c489b735bc997942f3c007fd2fd
eitan <eitan.vichik@gmail.com>
2022-01-24 14:25:31 +0200

    Delete unused commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.command_groups.Shoot;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.valuetuner.ValueTuner;
 import webapp.Webserver;
@@ -32,7 +31,6 @@ public class RobotContainer {
     }
 
     private void configureDefaultCommands() {
-        shooter.setDefaultCommand(new Shoot(shooter, xbox::getLeftY));
     }
 
     private void configureButtonBindings() {
