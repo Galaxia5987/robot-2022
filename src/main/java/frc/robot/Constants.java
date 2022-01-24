4470088ba333492efac4ca4924a452aebafd3def
eitan <eitan.vichik@gmail.com>
2022-01-24 14:41:56 +0200

    Change documentation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,7 +17,7 @@ public final class Constants {
         public static final double MODEL_TOLERANCE = 1e-6; // Model tolerance for state space.
         public static final double SENSOR_TOLERANCE = 1e-6; // Sensor tolerance for state space.
 
-        public static final double J = 0.00218; // Moment of inertia for state space. [kg*m^2]
+        public static final double J = 0.00218; // Moment of inertia for state space. This will be changed if the system will be in rps. [kg*m^2]
         public static final double GEAR_RATIO = 1; // Gear ratio for encoder.
         public static final double NEUTRAL_DEADBAND = 0.1; // [%]
 
--- src/main/java/frc/robot/command_groups/Outtake.java
+++ src/main/java/frc/robot/command_groups/Outtake.java
@@ -9,10 +9,6 @@ import java.util.function.DoubleSupplier;
 public class Outtake extends ParallelCommandGroup {
 
     public Outtake(Shooter shooter, DoubleSupplier distance) {
-        /*
-        Once the rest of the robot is operational, another command will
-        be added to feed the balls to the shooter.
-         */
         addCommands(
                 new Shoot(shooter, () -> shooter.getSetpointVelocity(distance.getAsDouble()))
         );
