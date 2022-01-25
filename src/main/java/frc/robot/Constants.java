ad2af7c5419a66a6cf9c8e9d7abc5f4025ae5f3a
eitan <eitan.vichik@gmail.com>
2022-01-25 17:45:37 +0200

    Delete unused constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,8 +3,4 @@ package frc.robot;
 
 public final class Constants {
 
-    public static class Hood {
-        public static final double CLOSED_ANGLE = 54; // Angle for longer distance. [deg]
-        public static final double OPEN_ANGLE = 69; // Angle for shorter distance. [deg]
-    }
 }
