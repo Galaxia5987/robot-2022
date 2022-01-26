7ca57f262e4801401e5e930f53f8fc49b22bc209
Barel Shlidor <Barel@shlidor.org>
2022-01-26 19:34:48 +0200

    Remove empty lines for Gaia

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,13 +1,10 @@
 package frc.robot;
 
-
 import frc.robot.valuetuner.WebConstant;
 
 public final class Constants {
-
     public static class ExampleSubsystem {
         private static final String NAME = ExampleSubsystem.class.getName();
         public static final WebConstant POWER = WebConstant.of(NAME, "power", 0.5); // [%]
     }
-
 }
