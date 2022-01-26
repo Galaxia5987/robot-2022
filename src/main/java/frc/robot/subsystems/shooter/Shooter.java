3fe24ef933cce67508ad7064ccbd8db88851c4d7
eitan <eitan.vichik@gmail.com>
2022-01-26 18:53:45 +0200

    Delete unused import

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -15,7 +15,6 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.subsystems.UnitModel;
 
 import static frc.robot.Constants.*;
