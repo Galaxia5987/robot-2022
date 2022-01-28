49007cb0a6030fed812844a0f2558cd5f4d75724
eitan <eitan.vichik@gmail.com>
2022-01-28 15:17:31 +0200

    Add unit documentation and constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,16 +3,16 @@ package frc.robot;
 
 public final class Constants {
 
-    public static final double NOMINAL_VOLTAGE = 12;
+    public static final double NOMINAL_VOLTAGE = 12; // [V]
 
     public static class ExampleSubsystem {
         public static final double POWER = 0.5; // [%]
     }
 
     public static class Intake {
-        public static final double POWER = 0.5; // [%], power intake will receive on the basic command
-        public static final double SLOPE = -3./16; // [% / m/s], 0.5 is a temp number (still hasnt been tested).
-        public static final double BIAS = 1; //[m/s]
-
+        public static final double POWER = 0.5; // power intake will receive on the basic command. [%]
+        public static final double SLOPE = -3./16; // 0.5 is a temp number (still hasnt been tested). [% / m/s]
+        public static final double BIAS = 1; // Maximal power of the intake. [%]
+        public static final boolean IS_COMPENSATING_VOLTAGE = true;
     }
 }
