4ae4644b2b0560a32c6f1a03dc1cd7a06b3cb23b
eitan <eitan.vichik@gmail.com>
2022-01-30 21:16:27 +0200

    Add more options to queue
    Also remove recommendations

--- src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
@@ -4,11 +4,11 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
-import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Ports;
 
 import java.util.Arrays;
@@ -22,8 +22,8 @@ public class Conveyor extends SubsystemBase {
     private final WPI_TalonFX motor = new WPI_TalonFX(Ports.Conveyor.MOTOR);
     private final Deque<String> position = new LinkedList<>();
     private final ColorSensorV3 colorSensorIntake = new ColorSensorV3(I2C.Port.kOnboard);
-    private final DigitalInput postFlapBeam = new DigitalInput(Ports.Conveyor.BEAM_BREAKER1);
-    private final DigitalInput preFlapBeam = new DigitalInput(Ports.Conveyor.BEAM_BREAKER2);
+    private final DigitalInput postFlapBeam = new DigitalInput(Ports.Conveyor.POST_FLAP_BEAM_BREAKER);
+    private final DigitalInput preFlapBeam = new DigitalInput(Ports.Conveyor.PRE_FLAP_BEAM_BREAKER);
     private final Solenoid flap = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Conveyor.SOLENOID);
     private final ColorMatch match = new ColorMatch();
     private DriverStation.Alliance lastSeenColor = DriverStation.Alliance.Invalid;
@@ -31,11 +31,9 @@ public class Conveyor extends SubsystemBase {
 
     private Conveyor() {
         motor.setInverted(INVERSION);
-        match.addColorMatch(Color.kRed); // red
-        match.addColorMatch(Color.kBlue); // blue
-        match.addColorMatch(Color.kGreen);
-        match.addColorMatch(Color.kYellow);
-        match.addColorMatch(Color.kBlack);
+        match.addColorMatch(Constants.Conveyor.RED);
+        match.addColorMatch(Constants.Conveyor.BLUE);
+        match.addColorMatch(Constants.Conveyor.NONE);
     }
 
     /**
@@ -51,7 +49,7 @@ public class Conveyor extends SubsystemBase {
     }
 
     /**
-     * @return the color sensor value as a DriverStation.Alliance enum
+     * @return the color sensor value as a DriverStation.Alliance enum.
      */
     public DriverStation.Alliance getColor() {
         Color color = colorSensorIntake.getColor();
@@ -59,9 +57,9 @@ public class Conveyor extends SubsystemBase {
         Color resultColor = result.color;
         SmartDashboard.putBoolean("match", resultColor == Color.kRed);
 
-        if (resultColor == Color.kRed) {
+        if (resultColor == Constants.Conveyor.RED) {
             return DriverStation.Alliance.Red;
-        } else if (resultColor == Color.kBlue) {
+        } else if (resultColor == Constants.Conveyor.BLUE) {
             return DriverStation.Alliance.Blue;
         } else {
             return DriverStation.Alliance.Invalid;
@@ -69,7 +67,7 @@ public class Conveyor extends SubsystemBase {
     }
 
     /**
-     * @returns the amount of balls inside the cargo
+     * @return the amount of balls inside the cargo.
      */
     public int getCargoCount() {
         return position.size();
@@ -82,17 +80,6 @@ public class Conveyor extends SubsystemBase {
      */
     public void setPower(double power) {
         motor.set(power);
-    }
-
-    /**
-     * @param first the cargo place for which to get the color, true - first, false - last.
-     * @returns the color of first/last cargos inside the conveyor.
-     */
-    public DriverStation.Alliance getColorPosition(boolean first) {
-        if (first) {
-            return DriverStation.Alliance.valueOf(position.getFirst());
-        }
-        return DriverStation.Alliance.valueOf(position.getLast());
     }
 
     /**
@@ -116,33 +103,18 @@ public class Conveyor extends SubsystemBase {
         flap.toggle();
     }
 
-    public enum Queue{
-        AllianceOpponent("Special case", new boolean[]{true, false}),
-        Alliance("Shoot", new boolean[]{true}),
-        OpponentAlliance("Outtake one ball", new boolean[]{false, true}),
-        Opponent("Outtake", new boolean[]{false}),
-        None("I don't know man", new boolean[]{});
-
-        public final String recommendedAction;
-        public final boolean[] queue;
-        Queue(String recommendedAction, boolean[] queue){
-            this.recommendedAction = recommendedAction;
-            this.queue = queue;
-        }
-    }
-
-    private void updateActualBallPositions(){
+    private void updateActualBallPositions() {
         var colorIntake = getColor();
-        boolean currentPostFlapBeamInput = postFlapBeam.get(); // post
+        boolean currentPostFlapBeamInput = postFlapBeam.get();
         SmartDashboard.putString("alliance", colorIntake.name());
         if (currentPostFlapBeamInput && !lastPostFlapBeamInput && motor.getMotorOutputPercent() > 0) {
             position.removeFirst();
         }
-        if (colorIntake == DriverStation.Alliance.Invalid && lastSeenColor != DriverStation.Alliance.Invalid) {
+        if (colorIntake != lastSeenColor) {
             if (motor.getMotorOutputPercent() < 0) {
                 position.removeLast();
             } else if (motor.getMotorOutputPercent() > 0) {
-                position.add(lastSeenColor.name());
+                position.add(colorIntake.name());
             }
         }
         lastPostFlapBeamInput = currentPostFlapBeamInput;
@@ -152,32 +124,42 @@ public class Conveyor extends SubsystemBase {
 
     private Queue getQueue() {
         /*
-        Case #1  queue = [Alliance, Opponent] - the special case.
-        Case #2  queue = [Alliance]
-        Case #3  queue = [Alliance, Alliance]
-        Case #4  queue = [Opponent, Alliance]
-        Case #5  queue = [Opponent]
-        Case #6  queue = [Opponent, Opponent]
+        Case #1  queue = [None, None]
+        Case #2  queue = [None, Alliance]
+        Case #3  queue = [Alliance, None]
+        Case #4  queue = [Alliance, Alliance]
+        Case #5  queue = [Alliance, Opponent]
+        Case #6  queue = [None, Opponent]
+        Case #7  queue = [Opponent, None]
+        Case #8  queue = [Opponent, Opponent]
+        Case #9  queue = [Opponent, Alliance]
          */
-        if(!position.getLast().equals(DriverStation.getAlliance().name())) {
-            if(!position.getFirst().equals(DriverStation.getAlliance().name())) {
-                return Queue.Opponent; // Case #6 and #5
-            } else if(position.getFirst().equals(DriverStation.getAlliance().name())) {
-                return Queue.OpponentAlliance; // Case #4
-            } else {
-                return Queue.None;
+        var last = position.getLast();
+        var first = position.getFirst();
+        var invalid = DriverStation.Alliance.Invalid.name();
+        var alliance = DriverStation.getAlliance().name();
+
+        if (last.equals(invalid)) {
+            if (first.equals(invalid)) {
+                return Queue.NoneNone;
+            } else if (first.equals(alliance)) {
+                return Queue.NoneAlliance;
             }
-        } else if(position.getLast().equals(DriverStation.getAlliance().name())) {
-            if(position.getLast().equals(DriverStation.getAlliance().name())) {
-                return Queue.Alliance; // Case #3 and #2
-            } else if (!position.getLast().equals(DriverStation.getAlliance().name())) {
-                return Queue.AllianceOpponent; // Case #1
-            } else {
-                return Queue.None;
+            return Queue.NoneOpponent;
+        } else if (last.equals(alliance)) {
+            if (first.equals(invalid)) {
+                return Queue.AllianceNone;
+            } else if (first.equals(alliance)) {
+                return Queue.AllianceAlliance;
             }
-        } else {
-            return Queue.None;
+            return Queue.AllianceOpponent;
         }
+        if (first.equals(invalid)) {
+            return Queue.OpponentNone;
+        } else if (first.equals(alliance)) {
+            return Queue.OpponentAlliance;
+        }
+        return Queue.OpponentOpponent;
     }
 
     /**
@@ -194,5 +176,23 @@ public class Conveyor extends SubsystemBase {
     public void simulationPeriodic() {
         SmartDashboard.putString("position", position.toString());
         SmartDashboard.putNumber("power", motor.get());
+    }
+
+    public enum Queue {
+        NoneNone(new int[]{0, 0}),
+        NoneAlliance(new int[]{0, 1}),
+        AllianceNone(new int[]{1, 0}),
+        AllianceAlliance(new int[]{1, 1}),
+        AllianceOpponent(new int[]{1, 2}),
+        NoneOpponent(new int[]{0, 2}),
+        OpponentNone(new int[]{2, 0}),
+        OpponentOpponent(new int[]{2, 2}),
+        OpponentAlliance(new int[]{2, 1});
+
+        public final int[] queue;
+
+        Queue(int[] queue) {
+            this.queue = queue;
+        }
     }
 }
