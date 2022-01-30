3a830b0440d3d8812b14355d086ed460860c6a6c
eitan <eitan.vichik@gmail.com>
2022-01-30 21:16:37 +0200

    Move colors to constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,9 +1,14 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.util.Color;
+
 public class Constants {
 
     public static class Conveyor {
         public static final double POWER = 0.5; // [%]
         public static final int MAX_CARGO_AMOUNT = 2;
+        public static final Color BLUE = new Color(0, 0, 0);
+        public static final Color RED = new Color(0, 0, 0);
+        public static final Color NONE = new Color(0, 0, 0);
     }
 }
