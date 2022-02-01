af4c27d731b82caf4caf4e81e91494cb93c620ef
eitan <eitan.vichik@gmail.com>
2022-02-01 19:29:37 +0200

    Add space
    It just looked bad

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static class Conveyor {
         public static final double POWER = 0.5; // [%]
         public static final int MAX_CARGO_AMOUNT = 2;
-        public static final Color BLUE = new Color(0,0, 1);
+        public static final Color BLUE = new Color(0, 0, 1);
         public static final Color RED = new Color(1, 0, 0);
         public static final Color NONE = new Color(0, 0, 0);
     }
