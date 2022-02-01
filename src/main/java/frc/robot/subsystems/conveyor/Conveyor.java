ae2b988253fe88c2ec65bf60767a387352cc27e9
eitan <eitan.vichik@gmail.com>
2022-02-01 13:52:53 +0200

    Simplify logic

--- src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
@@ -127,7 +127,7 @@ public class Conveyor extends SubsystemBase {
         }
         if (colorIntake != lastSeenColor && !colorIntake.equals(DriverStation.Alliance.Invalid)) {
             if (power < 0) {
-                if (cargoPositions.remainingCapacity() == 0) {
+                if (cargoPositions.isEmpty()) {
                     cargoPositions.poll();
                 }
                 cargoPositions.offer(colorIntake.name());
