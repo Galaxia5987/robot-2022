f73f4a2ec73ee223abd86e3ec738f1f162bf5135
eitan <eitan.vichik@gmail.com>
2022-02-04 11:18:43 +0200

    Add feed command

---
+++ src/main/java/frc/robot/subsystems/conveyor/commands/Feed.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems.conveyor.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.conveyor.Conveyor;
+
+public class Feed extends CommandBase {
+    private final double power;
+    private final Conveyor conveyor;
+
+    public Feed(double power, Conveyor conveyor) {
+        this.power = power;
+        this.conveyor = conveyor;
+    }
+
+    @Override
+    public void execute() {
+        if(power > 0) {
+            conveyor.openFlap();
+        } else {
+            conveyor.closeFlap();
+        }
+        conveyor.setPower(power);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        conveyor.setPower(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return !conveyor.isPreFlapBeamConnected();
+    }
+}
