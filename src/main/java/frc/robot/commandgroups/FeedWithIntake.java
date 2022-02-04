8d7c27aea9b3c53b7e22bd702dd908eca3258f50
eitan <eitan.vichik@gmail.com>
2022-02-04 11:53:34 +0200

    Change intake command in group

--- src/main/java/frc/robot/commandgroups/FeedWithIntake.java
+++ src/main/java/frc/robot/commandgroups/FeedWithIntake.java
@@ -5,15 +5,15 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.Feed;
 import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.intake.commands.IntakeByRobotSpeed;
+import frc.robot.subsystems.intake.commands.IntakeCargo;
 
 public class FeedWithIntake extends ParallelCommandGroup {
 
-    public FeedWithIntake(Conveyor conveyor, Intake intake, double conveyorPower) {
+    public FeedWithIntake(Conveyor conveyor, Intake intake, double conveyorPower, double intakePower) {
         addCommands(
                 new Feed(conveyorPower, conveyor),
                 new ConditionalCommand(
-                        new IntakeByRobotSpeed(intake, () -> 1),
+                        new IntakeCargo(intake, () -> true, intakePower),
                         null,
                         () -> conveyor.getCargoCount() < 2)
         );
