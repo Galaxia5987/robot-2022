b718ff25a359920c1c78f67c6b6893efa0650cd1
eitan <eitan.vichik@gmail.com>
2022-02-04 10:24:02 +0200

    Add documentation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static class Conveyor {
         public static final double POWER = 0.5; // [%]
         public static final int MAX_CARGO_AMOUNT = 2;
-        public static final int MIN_PROXIMITY_VALUE = 100;
+        public static final int MIN_PROXIMITY_VALUE = 100; // Minimum distance from the color sensor in order to induce detection (arbitrary bit units).
         public static final Color RED = new Color(0.15, 0.55, 0.3);
         public static final Color BLUE = new Color(0.02, 0.51, 0.44);
         public static final Color GREEN = new Color(0.06, 0.54, 0.39);
