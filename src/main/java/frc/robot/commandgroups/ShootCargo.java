24a3962abad7489565911821f08ea3369eecff41
eitan <eitan.vichik@gmail.com>
2022-02-04 12:46:08 +0200

    Change shoot cargo logic

--- src/main/java/frc/robot/commandgroups/ShootCargo.java
+++ src/main/java/frc/robot/commandgroups/ShootCargo.java
@@ -4,22 +4,36 @@ import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.Feed;
+import frc.robot.subsystems.conveyor.commands.SetFlapMode;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.commands.Shoot;
 import frc.robot.utils.Utils;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 import static frc.robot.Constants.CommandGroups.SHOOTER_VELOCITY_DEADBAND;
 
 public class ShootCargo extends ParallelCommandGroup {
 
-    public ShootCargo(Shooter shooter, Conveyor conveyor, DoubleSupplier distanceFromTarget, double conveyorPower) {
+    public ShootCargo(Shooter shooter,
+                      Conveyor conveyor,
+                      DoubleSupplier distanceFromTarget,
+                      double conveyorPower) {
+        final BooleanSupplier isAtSetpoint =
+                () -> Utils.deadband(
+                        shooter.getVelocity() / Shoot.getSetpointVelocity(
+                                distanceFromTarget.getAsDouble()) - 1, SHOOTER_VELOCITY_DEADBAND) == 0;
+
         addCommands(
+                new ConditionalCommand(
+                        new SetFlapMode(conveyor, Conveyor.FlapMode.Open),
+                        new SetFlapMode(conveyor, Conveyor.FlapMode.Closed),
+                        isAtSetpoint),
                 new ConditionalCommand(
                         new Feed(conveyorPower, conveyor),
                         null,
-                        () -> Utils.deadband(shooter.getVelocity() / Shoot.getSetpointVelocity(distanceFromTarget.getAsDouble()), SHOOTER_VELOCITY_DEADBAND) == 0),
+                        isAtSetpoint),
                 new Shoot(shooter, distanceFromTarget)
         );
     }
