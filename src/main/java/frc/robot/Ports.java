32b35967d6372c45768fc4a442a997910725db98
Saar <saarzi2004@gmail.com>
2022-02-05 17:31:41 +0200

    Update new ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,29 +4,29 @@ public final class Ports {
 
     public static final class SwerveDrive {
         // front right
-        public static final int DRIVE_MOTOR_FR = 23;
-        public static final int ANGLE_MOTOR_FR = 24;
+        public static final int DRIVE_MOTOR_FR = 1;
+        public static final int ANGLE_MOTOR_FR = 2;
         public static final boolean DRIVE_INVERTED_FR = false;
         public static final boolean ANGLE_INVERTED_FR = false;
         public static final boolean ANGLE_SENSOR_PHASE_FR = true;
 
         // front left
-        public static final int DRIVE_MOTOR_FL = 27;
-        public static final int ANGLE_MOTOR_FL = 28;
+        public static final int DRIVE_MOTOR_FL = 5;
+        public static final int ANGLE_MOTOR_FL = 6;
         public static final boolean DRIVE_INVERTED_FL = false;
         public static final boolean ANGLE_INVERTED_FL = true;
         public static final boolean ANGLE_SENSOR_PHASE_FL = false;
 
         // rear right
-        public static final int DRIVE_MOTOR_RR = 21;
-        public static final int ANGLE_MOTOR_RR = 22;
+        public static final int DRIVE_MOTOR_RR = 3;
+        public static final int ANGLE_MOTOR_RR = 4;
         public static final boolean DRIVE_INVERTED_RR = false;
         public static final boolean ANGLE_INVERTED_RR = true;
         public static final boolean ANGLE_SENSOR_PHASE_RR = false;
 
         // rear left
-        public static final int DRIVE_MOTOR_RL = 25;
-        public static final int ANGLE_MOTOR_RL = 26;
+        public static final int DRIVE_MOTOR_RL = 6;
+        public static final int ANGLE_MOTOR_RL = 7;
         public static final boolean DRIVE_INVERTED_RL = false;
         public static final boolean ANGLE_INVERTED_RL = true;
         public static final boolean ANGLE_SENSOR_PHASE_RL = false;
