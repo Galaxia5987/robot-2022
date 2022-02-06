c1b18ca48887160a55c3b647cba21a3d90af6af8
eitan <eitan.vichik@gmail.com>
2022-02-06 18:09:03 +0200

    Add manual control for shooter (bit)

---
+++ src/main/java/frc/robot/subsystems/shooter/commands/ManualFlywheelControl.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.shooter.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.shooter.Shooter;
+
+import java.util.function.DoubleSupplier;
+
+public class ManualFlywheelControl extends CommandBase {
+    private final DoubleSupplier joystickY;
+    private final Shooter shooter;
+
+    public ManualFlywheelControl(DoubleSupplier joystickY, Shooter shooter) {
+        this.joystickY = joystickY;
+        this.shooter = shooter;
+    }
+
+    @Override
+    public void execute() {
+        shooter.setPower(Constants.Shooter.OUTPUT_MULTIPLIER * -joystickY.getAsDouble());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if(interrupted) {
+            System.out.println("I've been shanked!");
+        }
+        shooter.setPower(0);
+    }
+}
