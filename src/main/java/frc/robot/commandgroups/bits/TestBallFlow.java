c05a27115643f5816a3c188f76fbf1ae5669ea95
eitan <eitan.vichik@gmail.com>
2022-02-08 20:52:52 +0200

    Execute super method

--- src/main/java/frc/robot/commandgroups/bits/TestBallFlow.java
+++ src/main/java/frc/robot/commandgroups/bits/TestBallFlow.java
@@ -36,6 +36,8 @@ public class TestBallFlow extends SequentialCommandGroup {
 
     @Override
     public void execute() {
+        super.execute();
+
         if (pickUpCargo.isScheduled()) {
             SmartDashboard.putNumber("Intake power", intake.getPower());
         } else if (shootCargo.isScheduled()) {
