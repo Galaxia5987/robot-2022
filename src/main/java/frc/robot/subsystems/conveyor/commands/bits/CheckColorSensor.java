3c56268e9e7dae0abd0aa04c6bcaa4585f2d1622
eitan <eitan.vichik@gmail.com>
2022-02-08 17:06:26 +0200

    Make test for color sensor

---
+++ src/main/java/frc/robot/subsystems/conveyor/commands/bits/CheckColorSensor.java
@@ -0,0 +1,50 @@
+package frc.robot.subsystems.conveyor.commands.bits;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.conveyor.Conveyor;
+import frc.robot.subsystems.conveyor.commands.Convey;
+
+import java.util.function.DoubleSupplier;
+
+public class CheckColorSensor extends Convey {
+    private final DoubleSupplier direction;
+    private double lastDirection;
+    private double maximalNumberOfBalls = 0;
+
+    public CheckColorSensor(Conveyor conveyor, DoubleSupplier direction, double power) {
+        super(conveyor, () -> power * direction.getAsDouble());
+        this.direction = direction;
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+
+        var queue = conveyor.getQueue();
+        double currentDirection = -direction.getAsDouble();
+
+        if(currentDirection > 0) {
+            SmartDashboard.putString("First", queue.getFirst());
+            SmartDashboard.putString("Last", queue.getLast());
+            SmartDashboard.putNumber("Number of cargo", conveyor.getCargoCount());
+        } else if(currentDirection < 0) {
+            if (currentDirection != lastDirection) {
+                maximalNumberOfBalls = conveyor.getCargoCount();
+            } else {
+                SmartDashboard.putString("First", queue.getFirst());
+                SmartDashboard.putString("Last", queue.getLast());
+                SmartDashboard.putNumber("Number of cargo", conveyor.getCargoCount());
+            }
+        } else if(maximalNumberOfBalls > 0) {
+            SmartDashboard.putNumberArray("[In, Out]",
+                    new double[]{conveyor.getCargoCount(), maximalNumberOfBalls});
+        }
+
+        lastDirection = currentDirection;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+    }
+}
