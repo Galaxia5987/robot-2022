ba73151fcbd5f91b1fecdb5f8645d0b3131f5688
Barel Shlidor <Barel@shlidor.org>
2022-02-09 11:17:13 +0200

    Reformat files

--- src/main/java/webapp/StateApplets.java
+++ src/main/java/webapp/StateApplets.java
@@ -6,14 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class StateApplets {
-  public static class LatestState extends HttpServlet {
-    private static final long serialVersionUID = 7888425758718454864L;
+    public static class LatestState extends HttpServlet {
+        private static final long serialVersionUID = 7888425758718454864L;
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-      res.setContentType("application/json");
-      res.setStatus(HttpServletResponse.SC_OK);
-      res.getWriter().println(FireLog.toJsonObject().toString());
+        protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+            res.setContentType("application/json");
+            res.setStatus(HttpServletResponse.SC_OK);
+            res.getWriter().println(FireLog.toJsonObject());
+        }
     }
-  }
 }
  
