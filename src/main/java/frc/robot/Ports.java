31411693a0ba4da503073b86a47f9a5f8859d46f
yuvallaif <71329836+Se-gol@users.noreply.github.com>
2022-02-13 20:48:16 +0200

    Fix inversions

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -9,26 +9,26 @@ public final class Ports {
         public static final int DRIVE_MOTOR_FR = 1;
         public static final int ANGLE_MOTOR_FR = 2;
         public static final boolean DRIVE_INVERTED_FR = false;
-        public static final boolean ANGLE_INVERTED_FR = false;
-        public static final boolean ANGLE_SENSOR_PHASE_FR = true;
+        public static final boolean ANGLE_INVERTED_FR = true;
+        public static final boolean ANGLE_SENSOR_PHASE_FR = false;
 
         // front left
-        public static final int DRIVE_MOTOR_FL = 5;
-        public static final int ANGLE_MOTOR_FL = 6;
+        public static final int DRIVE_MOTOR_FL = 3;
+        public static final int ANGLE_MOTOR_FL = 4;
         public static final boolean DRIVE_INVERTED_FL = false;
-        public static final boolean ANGLE_INVERTED_FL = true;
-        public static final boolean ANGLE_SENSOR_PHASE_FL = false;
+        public static final boolean ANGLE_INVERTED_FL = false; // true
+        public static final boolean ANGLE_SENSOR_PHASE_FL = true;
 
         // rear right
-        public static final int DRIVE_MOTOR_RR = 3;
-        public static final int ANGLE_MOTOR_RR = 4;
+        public static final int DRIVE_MOTOR_RR = 5;
+        public static final int ANGLE_MOTOR_RR = 6;
         public static final boolean DRIVE_INVERTED_RR = false;
         public static final boolean ANGLE_INVERTED_RR = true;
         public static final boolean ANGLE_SENSOR_PHASE_RR = false;
 
         // rear left
-        public static final int DRIVE_MOTOR_RL = 6;
-        public static final int ANGLE_MOTOR_RL = 7;
+        public static final int DRIVE_MOTOR_RL = 7;
+        public static final int ANGLE_MOTOR_RL = 8;
         public static final boolean DRIVE_INVERTED_RL = false;
         public static final boolean ANGLE_INVERTED_RL = true;
         public static final boolean ANGLE_SENSOR_PHASE_RL = false;
