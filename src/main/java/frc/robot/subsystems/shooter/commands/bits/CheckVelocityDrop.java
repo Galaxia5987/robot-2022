4aef0cc69718fb21885eb5ce1a6f255bb5b7f8aa
eitan <eitan.vichik@gmail.com>
2022-02-15 17:50:01 +0200

    Add velocity drop bit
    Recommended to start with this bit when testing the shooter.

---
+++ src/main/java/frc/robot/subsystems/shooter/commands/bits/CheckVelocityDrop.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems.shooter.commands.bits;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.commands.Shoot;
+import webapp.FireLog;
+
+import java.util.OptionalDouble;
+import java.util.function.DoubleSupplier;
+
+public class CheckVelocityDrop extends Shoot {
+    private double lastVelocity;
+    private double maximalDrop = 0;
+
+    public CheckVelocityDrop(Shooter shooter, DoubleSupplier distance, OptionalDouble power) {
+        super(shooter, distance, power);
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+        double velocity = shooter.getVelocity();
+        double setpoint = getSetpointVelocity(distance.getAsDouble());
+        maximalDrop = Math.max(maximalDrop, lastVelocity - velocity);
+
+        FireLog.log("Setpoint", setpoint);
+        FireLog.log("Actual velocity", velocity);
+        FireLog.log("Maximal drop", maximalDrop);
+        FireLog.log("Percent drop", (maximalDrop / setpoint) * 100);
+
+        lastVelocity = velocity;
+    }
+}
