a7b6f1489786f504f7091baf93b985c25b52a6f7
Gaia Zano <gaya.zano11@gmail.com>
2022-02-16 18:01:38 +0200

    Add paths
    They are safe!

---
+++ src/main/java/frc/robot/autoPaths/Path4.java
@@ -0,0 +1,58 @@
+package frc.robot.autoPaths;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commandgroups.PickUpCargo;
+import frc.robot.commandgroups.ShootCargo;
+import frc.robot.subsystems.conveyor.Conveyor;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.flap.Flap;
+import frc.robot.subsystems.hood.Hood;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.Function;
+
+public class Path4 extends SequentialCommandGroup {
+    private DoubleSupplier distanceFromTarget;
+    private DoubleSupplier conveyorPower;
+
+    public Path4(Shooter shooter, SwerveDrive swerveDrive, Conveyor conveyor, Intake intake, Hood hood, Flap flap) {
+        var rotationPID = new ProfiledPIDController(Constants.Autonomous.KP_THETA_CONTROLLER, 0, 0, new TrapezoidProfile.Constraints(Constants.Autonomous.MAX_VEL, Constants.Autonomous.MAX_ACCEL));
+        rotationPID.enableContinuousInput(-Math.PI, Math.PI);
+
+        Function<String, PPSwerveControllerCommand> createCommand = path ->  new PPSwerveControllerCommand(
+                PathPlanner.loadPath(path, Constants.Autonomous.MAX_VEL, Constants.Autonomous.MAX_ACCEL),
+                swerveDrive::getPose,
+                swerveDrive.getKinematics(),
+                new PIDController(Constants.Autonomous.KP_X_CONTROLLER, 0, 0),
+                new PIDController(Constants.Autonomous.KP_Y_CONTROLLER, 0, 0),
+                rotationPID,
+                swerveDrive::setStates,
+                swerveDrive);
+
+        addCommands(createCommand.apply("p1 - Taxi from low right and pickup low cargo(4.1)"));
+        addCommands(new PickUpCargo(
+                conveyor,
+                intake,
+                Constants.Conveyor.DEFAULT_POWER,
+                Constants.Intake.DEFAULT_POWER
+        ));
+        addCommands(new ShootCargo(
+                shooter,
+                hood,
+                conveyor,
+                flap,
+                distanceFromTarget,
+                conveyorPower
+        ));
+        addCommands(createCommand.apply("p1 - Going to low tarmac(3.2.1)"));
+    }
+}
+
---
+++ src/main/java/frc/robot/autoPaths/TaxiFromUpUpAndShoot.java
@@ -0,0 +1,59 @@
+package frc.robot.autoPaths;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commandgroups.ShootCargo;
+import frc.robot.subsystems.conveyor.Conveyor;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.flap.Flap;
+import frc.robot.subsystems.hood.Hood;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+
+import java.util.function.DoubleSupplier;
+
+public class TaxiFromUpUpAndShoot extends SequentialCommandGroup {
+    private DoubleSupplier distanceFromTarget;
+    private DoubleSupplier conveyorPower;
+
+    //Taxi from up up tarmac, shoot pre-loaded cargo, park near up tarmac.
+
+    public TaxiFromUpUpAndShoot(Shooter shooter, SwerveDrive swerveDrive, Conveyor conveyor, Intake intake, Hood hood, Flap flap) {
+        var rotationPID = new ProfiledPIDController(Constants.Autonomous.KP_THETA_CONTROLLER, 0, 0, new TrapezoidProfile.Constraints(Constants.Autonomous.MAX_VEL, Constants.Autonomous.MAX_ACCEL));
+        rotationPID.enableContinuousInput(-Math.PI, Math.PI);
+
+        var taxiFromUpUpTarmac = PathPlanner.loadPath("p0 - Taxi from up up tarmac(2.1)", Constants.Autonomous.MAX_VEL, Constants.Autonomous.MAX_ACCEL);
+        new PPSwerveControllerCommand(taxiFromUpUpTarmac,
+                swerveDrive::getPose,
+                swerveDrive.getKinematics(),
+                new PIDController(Constants.Autonomous.KP_X_CONTROLLER, 0, 0),
+                new PIDController(Constants.Autonomous.KP_Y_CONTROLLER, 0, 0),
+                rotationPID,
+                swerveDrive::setStates,
+                swerveDrive);
+
+        addCommands(new ShootCargo(
+                shooter,
+                hood,
+                conveyor,
+                flap,
+                distanceFromTarget,
+                conveyorPower
+        ));
+
+        var goToUpTarmac = PathPlanner.loadPath("p0 - Go to up tarmac(1.2.2)", Constants.Autonomous.MAX_VEL, Constants.Autonomous.MAX_ACCEL);
+        new PPSwerveControllerCommand(goToUpTarmac,
+                swerveDrive::getPose,
+                swerveDrive.getKinematics(),
+                new PIDController(Constants.Autonomous.KP_X_CONTROLLER, 0, 0),
+                new PIDController(Constants.Autonomous.KP_Y_CONTROLLER, 0, 0),
+                rotationPID,
+                swerveDrive::setStates,
+                swerveDrive);
+    }
+}
