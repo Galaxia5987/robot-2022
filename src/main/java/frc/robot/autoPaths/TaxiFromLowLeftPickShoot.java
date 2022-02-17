6e7843186c5a2e4a170002193a60a1a3bb53cd1a
Gaia Zano <gaya.zano11@gmail.com>
2022-02-17 20:39:04 +0200

    Add Path.

---
+++ src/main/java/frc/robot/autoPaths/TaxiFromLowLeftPickShoot.java
@@ -0,0 +1,62 @@
+package frc.robot.autoPaths;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commandgroups.PickUpCargo;
+import frc.robot.commandgroups.ShootCargo;
+import frc.robot.subsystems.conveyor.Conveyor;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.flap.Flap;
+import frc.robot.subsystems.hood.Hood;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.Function;
+
+public class TaxiFromLowLeftPickShoot extends SequentialCommandGroup {
+    private DoubleSupplier distanceFromTarget;
+    private DoubleSupplier conveyorPower;
+
+    // Taxi from low left, pick up middle cargo, shoot, park between tarmacs.(3)
+    public TaxiFromLowLeftPickShoot(Shooter shooter, SwerveDrive swerveDrive, Conveyor conveyor, Intake intake, Hood hood, Flap flap) {
+        var rotationPID = new ProfiledPIDController(Constants.Autonomous.KP_THETA_CONTROLLER, 0, 0, new TrapezoidProfile.Constraints(Constants.Autonomous.MAX_VEL, Constants.Autonomous.MAX_ACCEL));
+        rotationPID.enableContinuousInput(-Math.PI, Math.PI);
+
+        Function<String, PPSwerveControllerCommand> createCommand = path -> new PPSwerveControllerCommand(
+                PathPlanner.loadPath(path, Constants.Autonomous.MAX_VEL, Constants.Autonomous.MAX_ACCEL),
+                swerveDrive::getPose,
+                swerveDrive.getKinematics(),
+                new PIDController(Constants.Autonomous.KP_X_CONTROLLER, 0, 0),
+                new PIDController(Constants.Autonomous.KP_Y_CONTROLLER, 0, 0),
+                rotationPID,
+                swerveDrive::setStates,
+                swerveDrive);
+
+        new ParallelCommandGroup((createCommand.apply("p1 - Taxi from low left and pickup middle cargo(3.1)")),
+                new PickUpCargo(
+                        conveyor,
+                        intake,
+                        Constants.Conveyor.DEFAULT_POWER,
+                        Constants.Intake.DEFAULT_POWER
+                ));
+
+
+        addCommands(new ShootCargo(
+                shooter,
+                hood,
+                conveyor,
+                flap,
+                distanceFromTarget,
+                conveyorPower
+        ));
+
+        addCommands(createCommand.apply("p1 - Going to middle tarmac(3.2.2))"));
+    }
+}
