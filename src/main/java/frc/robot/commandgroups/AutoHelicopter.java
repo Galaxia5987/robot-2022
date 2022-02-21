2068e2c3e9d6c9e81d09106cfd4c68ceefa3c105
Gaia Zano <gaya.zano11@gmail.com>
2022-02-21 20:35:58 +0200

    Add WaitCommand and MoveHelicopter command.

--- src/main/java/frc/robot/commandgroups/AutoHelicopter.java
+++ src/main/java/frc/robot/commandgroups/AutoHelicopter.java
@@ -1,6 +1,9 @@
 package frc.robot.commandgroups;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
 import frc.robot.subsystems.helicopter.Helicopter;
 import frc.robot.subsystems.helicopter.commands.AdjustAngle;
 import frc.robot.subsystems.helicopter.commands.MoveHelicopter;
@@ -11,6 +14,10 @@ public class AutoHelicopter extends SequentialCommandGroup {
     public AutoHelicopter(Helicopter helicopter,
                           double desiredRad) {
 
-        addCommands(new MoveHelicopter(helicopter, desiredRad));
+        addCommands(
+                new MoveHelicopter(helicopter, Constants.Helicopter.THIRD_RUNG),
+                new WaitCommand(1),
+                new MoveHelicopter(helicopter, Constants.Helicopter.RUNG_SEPARATION)
+        );
     }
 }
