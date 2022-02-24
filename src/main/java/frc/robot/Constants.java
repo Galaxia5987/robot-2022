26bf727860e3c3c7c95152286c3ede0648cd9978
Gaia Zano <gaya.zano11@gmail.com>
2022-02-24 16:57:51 +0200

    Add yaw constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -242,6 +242,9 @@ public final class Constants {
         public static final double THIRD_RUNG = 0; // [radians]
         public static final double RUNG_SEPARATION = 0; // [radians]
 
+        public static final double BLUE_RUNG_YAW = 180;
+        public static final double RED_RUNG_YAW = BLUE_RUNG_YAW - 180;
+
     }
 
     public static class Intake {
