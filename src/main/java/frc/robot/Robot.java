ee688d8279dbd73f7cfe136e4648c883b86bdd10
yuvallaif <Barel@shlidor.org>
2022-02-25 17:36:41 +0200

    Disable leds on disable

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.flap.Flap;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.valuetuner.NetworkTableConstant;
@@ -27,7 +26,7 @@ public class Robot extends TimedRobot {
     public static final boolean debug = !DriverStation.isFMSAttached();
     public static final AHRS navx = new AHRS(SPI.Port.kMXP);
     private static Rotation2d zeroAngle = new Rotation2d();
-//    private static Rotation2d startAngle = new Rotation2d();
+    //    private static Rotation2d startAngle = new Rotation2d();
     public PowerDistribution pdp = new PowerDistribution();
     private Command m_autonomousCommand;
     private RobotContainer m_robotContainer;
@@ -89,7 +88,6 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotPeriodic() {
-//        System.out.println(SwerveDrive.getFieldOrientedInstance().getPose());
         CommandScheduler.getInstance().run();
     }
 
@@ -144,6 +142,7 @@ public class Robot extends TimedRobot {
     public void disabledInit() {
         Intake.getInstance().closeRetractor();
         Flap.getInstance().blockShooter();
+        m_robotContainer.photonVisionModule.setLeds(true);
     }
 
     /**
