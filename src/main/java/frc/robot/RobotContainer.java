d329a2441b603ab7ab7401d5f5b9b9f5ee1221c2
Saar <saarzi2004@gmail.com>
2022-02-27 12:59:47 +0200

    Fix compilation errors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,29 +8,27 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commandgroups.ShootCargo;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.Convey;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
-import frc.robot.subsystems.drivetrain.commands.OverpoweredDrive;
-import frc.robot.subsystems.drivetrain.commands.testing.SimpleAdjustWithVision;
+import frc.robot.subsystems.drivetrain.commands.DriveAndAdjustWithVision;
 import frc.robot.subsystems.flap.Flap;
 import frc.robot.subsystems.helicopter.Helicopter;
 import frc.robot.subsystems.helicopter.commands.JoystickPowerHelicopter;
-import frc.robot.subsystems.helicopter.commands.StopHelicopter;
 import frc.robot.subsystems.hood.Hood;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.utils.PhotonVisionModule;
 import webapp.Webserver;
 
+import java.util.function.DoubleSupplier;
+
 public class RobotContainer {
     private static final Joystick joystick = new Joystick(Ports.Controls.JOYSTICK);
     private static final Joystick joystick2 = new Joystick(Ports.Controls.JOYSTICK2);
-    final PhotonVisionModule photonVisionModule = new PhotonVisionModule("photonvision", null);
-    // The robot's subsystems and commands are defined here...
     private final XboxController xbox = new XboxController(Ports.Controls.XBOX);
     private final JoystickButton a = new JoystickButton(xbox, XboxController.Button.kA.value);
     private final JoystickButton b = new JoystickButton(xbox, XboxController.Button.kB.value);
@@ -39,9 +37,9 @@ public class RobotContainer {
     private final JoystickButton lb = new JoystickButton(xbox, XboxController.Button.kLeftBumper.value);
     private final JoystickButton leftTrigger = new JoystickButton(joystick, Joystick.ButtonType.kTrigger.value);
     private final JoystickButton rightTrigger = new JoystickButton(joystick2, Joystick.ButtonType.kTrigger.value);
-    //    private final JoystickButton rightButton = new JoystickButton(joystick2, 6);
     private final Trigger rt = new Trigger(() -> xbox.getRightTriggerAxis() > Constants.Control.RIGHT_TRIGGER_DEADBAND);
     private final Trigger lt = new Trigger(() -> xbox.getLeftTriggerAxis() > Constants.Control.RIGHT_TRIGGER_DEADBAND);
+
     // The robot's subsystems and commands are defined here...
     private final SwerveDrive swerve = SwerveDrive.getFieldOrientedInstance();
     private final Intake intake = Intake.getInstance();
@@ -49,9 +47,8 @@ public class RobotContainer {
     private final Flap flap = Flap.getInstance();
     private final Shooter shooter = Shooter.getInstance();
     private final Hood hood = Hood.getInstance();
-    private final PhotonVisionModule photonVisionModule = new PhotonVisionModule("photonvision", null);
     private final Helicopter helicopter = Helicopter.getInstance();
-//    private final DigitalOutput digitalOutput = new DigitalOutput(4);
+    final PhotonVisionModule photonVisionModule = new PhotonVisionModule("photonvision", null);
 
     /**
      * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -74,13 +71,9 @@ public class RobotContainer {
 
     private void configureButtonBindings() {
         DoubleSupplier distanceFromTarget = () -> photonVisionModule.getDistance().orElse(-Constants.Vision.TARGET_RADIUS) + Constants.Vision.TARGET_RADIUS;
-//        DoubleSupplier conveyorPower = Constants.Conveyor.DEFAULT_POWER::get;
-//        DoubleSupplier setpointVelocity = () -> Shoot.getSetpointVelocity(distanceFromTarget.getAsDouble(), hood.isOpen());
-//        BooleanSupplier isFlywheelAtSetpoint = () -> Math.abs(setpointVelocity.getAsDouble() - shooter.getVelocity()) < SHOOTER_VELOCITY_DEADBAND.get();
-//        b.whenPressed(new FlapForShooting(flap, isFlywheelAtSetpoint, () -> !conveyor.isPreFlapBeamConnected()));
         rt.whileActiveContinuous(new ShootCargo(shooter, hood, conveyor, flap, () -> Constants.Conveyor.SHOOT_POWER, distanceFromTarget));
         lt.whileActiveContinuous(new ParallelCommandGroup(
-                new Convey(conveyor,Constants.Conveyor.DEFAULT_POWER.get()),
+                new Convey(conveyor, Constants.Conveyor.DEFAULT_POWER.get()),
                 new InstantCommand(() -> flap.setFlapMode(Flap.FlapMode.STOP_CARGO))));
         b.whileHeld(new Convey(conveyor, -Constants.Conveyor.DEFAULT_POWER.get()));
         x.whenPressed(photonVisionModule::toggleLeds);
