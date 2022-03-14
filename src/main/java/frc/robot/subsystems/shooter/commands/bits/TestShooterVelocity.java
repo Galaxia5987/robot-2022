f4864244d6098505c954fcd1a72f90e8ee112df5
Gaia Zano <gaya.zano11@gmail.com>
2022-03-14 20:55:18 +0200

    Change the order of the checks

--- src/main/java/frc/robot/subsystems/shooter/commands/bits/TestShooterVelocity.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/bits/TestShooterVelocity.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.shooter.commands.bits;
 
-import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.shooter.Shooter;
 
 public class TestShooterVelocity extends SequentialCommandGroup {
@@ -8,13 +11,13 @@ public class TestShooterVelocity extends SequentialCommandGroup {
     public TestShooterVelocity(Shooter shooter) {
 
         addCommands(
-                new ParallelRaceGroup(
-                        new InstantCommand(
-                                () -> shooter.setVelocity(3000)).withTimeout(5),
-                        new RunCommand(
-                                () -> System.out.println("shooter vel3000: " + shooter.getVelocity()))),
+                        new ParallelRaceGroup(
+                                new InstantCommand(
+                                        () -> shooter.setVelocity(0)).withTimeout(5),
+                                new RunCommand(
+                                        () -> System.out.println("shooter vel0: " + shooter.getVelocity()))),
 
-                new ParallelRaceGroup(
+                    new ParallelRaceGroup(
                         new InstantCommand(
                                 () -> shooter.setVelocity(1500)).withTimeout(5)
                                 .alongWith(new RunCommand(
@@ -22,9 +25,9 @@ public class TestShooterVelocity extends SequentialCommandGroup {
 
                 new ParallelRaceGroup(
                         new InstantCommand(
-                                () -> shooter.setVelocity(0)).withTimeout(5),
+                                () -> shooter.setVelocity(3000)).withTimeout(5),
                         new RunCommand(
-                                () -> System.out.println("shooter vel0: " + shooter.getVelocity())
-                        )));
+                                () -> System.out.println("shooter vel3000: " + shooter.getVelocity()
+                                ))));
     }
 }
