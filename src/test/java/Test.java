c79dcb3fa827457a5f0ef7de525e60c4945f6c23
Saar <saarzi2004@gmail.com>
2022-03-15 13:08:50 +0200

    Delete test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,18 +1,2 @@
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-
 public class Test {
-    @org.junit.Test
-    public void test() {
-//        var robotAngle = Rotation2d.fromDegrees(0);
-//        var robotPose = new Translation2d(1, 1);
-//        var hubPose = new Translation2d(3, 3);
-//        var poseRelativeToTarget = hubPose.minus(robotPose);
-//        var value = robotAngle.minus(new Rotation2d(
-//                Math.atan2(
-//                        poseRelativeToTarget.getY(),
-//                        poseRelativeToTarget.getX()
-//                )));
-//        System.out.println(value.getDegrees());
-    }
 }
