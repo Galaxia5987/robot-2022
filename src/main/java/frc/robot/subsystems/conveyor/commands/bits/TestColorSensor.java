d87a423c70bd7bdf3a1eff0ed35af3f3ce8db2c4
Gaia Zano <gaya.zano11@gmail.com>
2022-03-15 16:17:48 +0200

    Add a command to close intake retractor

--- src/main/java/frc/robot/subsystems/conveyor/commands/bits/TestColorSensor.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/bits/TestColorSensor.java
@@ -4,17 +4,21 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.conveyor.Conveyor;
+import frc.robot.subsystems.intake.Intake;
 
 public class TestColorSensor extends SequentialCommandGroup {
 
-    public TestColorSensor(Conveyor conveyor) {
+    public TestColorSensor(Conveyor conveyor, Intake intake) {
         addCommands(
+                new RunCommand(intake::closeRetractor),
                 new RunCommand(() -> System.out.println("Enter blue cargo")).withTimeout(4),
-                new RunCommand(() -> checkColorBlue(conveyor)).withTimeout(10),
+                new RunCommand(() -> checkColorBlue(conveyor)).withInterrupt(() -> conveyor.getColor() == DriverStation.Alliance.Blue),
                 new RunCommand(() -> System.out.println("Enter red cargo")).withTimeout(4),
-                new RunCommand(() -> checkColorRed(conveyor)).withTimeout(10)
+                new RunCommand(() -> checkColorRed(conveyor)).withInterrupt(() -> conveyor.getColor() == DriverStation.Alliance.Red),
+                new RunCommand(() -> System.out.println("Sensor works!"))
         );
     }
+
 
     public static void checkColorBlue(Conveyor conveyor) {
         if (conveyor.getColor() != DriverStation.Alliance.Invalid) {
