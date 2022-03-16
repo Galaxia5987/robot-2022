c719c9706ed7a3d7fb2c0b6cd561a951a2d9bd55
Alon <alon.schwierz@gmail.com>
2022-03-16 15:44:57 +0200

    Test calculateCurrentGoal Passed

--- src/test/java/frc/robot/commandgroups/TestShootAndRun.java
+++ src/test/java/frc/robot/commandgroups/TestShootAndRun.java
@@ -9,10 +9,26 @@ import org.junit.Test;
 public class TestShootAndRun {
 
 
-
     @Test
     public void testCalculateCurrentGoal() {
+        //First quadrant
         assertEquals(new Translation2d(1, 0), ShootAndRun.calculateCurrentGoal(1, 0),
                 "Should give coordinates of (1, 0)");
+        assertEquals(new Translation2d(0,1), ShootAndRun.calculateCurrentGoal(1, 90),
+                "should give coordinates of (0,1)");
+        assertEquals(new Translation2d(1, 1), ShootAndRun.calculateCurrentGoal(Math.sqrt(2), 45),
+                "Should give coordinates of (1,1)");
+        //Second quadrant
+        assertEquals(new Translation2d(-1, 0), ShootAndRun.calculateCurrentGoal(1, 180),
+                "should give coordinates of (-1, 0)");
+        assertEquals(new Translation2d(-1, 1), ShootAndRun.calculateCurrentGoal(Math.sqrt(2), 135),
+                "should give coordinates of (-1, 1)");
+        //Third quadrant
+        assertEquals(new Translation2d(-1, -1), ShootAndRun.calculateCurrentGoal(Math.sqrt(2), 225),
+                "Should give coordinates of (-1 , -1)");
+        //Fourth quadrant
+        assertEquals(new Translation2d(1, -1), ShootAndRun.calculateCurrentGoal(Math.sqrt(2), 315),
+                "should give coordinates of (-1, 0)");
+
     }
 }
