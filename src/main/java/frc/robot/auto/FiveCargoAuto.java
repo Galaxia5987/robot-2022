26d135f20ae8109b9606c37a4c6450bccb26d45b
Saar <saarzi2004@gmail.com>
2022-03-20 17:44:33 +0200

    Auto stash before revert of "Merge stashed changes"

--- src/main/java/frc/robot/auto/FiveCargoAuto.java
+++ src/main/java/frc/robot/auto/FiveCargoAuto.java
@@ -1,5 +1,7 @@
 package frc.robot.auto;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.conveyor.Conveyor;
