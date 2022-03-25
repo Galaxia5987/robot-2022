6daf66299adebb068842212ac3801f3c38e5d4fa
Galaxia <Galaxia5987@gmail.com>
2022-03-25 10:03:54 +0300

    Add auto test

---
+++ src/main/java/frc/robot/auto/Test.java
@@ -0,0 +1,16 @@
+package frc.robot.auto;
+
+import frc.robot.subsystems.conveyor.Conveyor;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.flap.Flap;
+import frc.robot.subsystems.hood.Hood;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.utils.PhotonVisionModule;
+
+public class Test extends SaarIsAutonomous{
+    public Test(SwerveDrive swerveDrive, Shooter shooter, Conveyor conveyor, Intake intake, Hood hood, Flap flap, PhotonVisionModule visionModule) {
+        super(swerveDrive, shooter, conveyor, intake, hood, flap, visionModule, "Meter");
+        addCommands(followPath("Meter"));
+    }
+}
