060df3c8313b4694e271fa98a2efa325aa516b8e
Gaia Zano <Gaya.zano11@gmail.com>
2022-03-26 18:23:35 +0300

    Change path

--- src/main/java/frc/robot/auto/StraightLineFour.java
+++ src/main/java/frc/robot/auto/StraightLineFour.java
@@ -1,6 +1,6 @@
 package frc.robot.auto;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.flap.Flap;
@@ -12,16 +12,46 @@ import frc.robot.utils.PhotonVisionModule;
 public class StraightLineFour extends SaarIsAutonomous {
 
     public StraightLineFour(SwerveDrive swerveDrive, Shooter shooter, Conveyor conveyor, Intake intake, Hood hood, Flap flap, PhotonVisionModule visionModule) {
-        super(swerveDrive, shooter, conveyor, intake, hood, flap, visionModule, "");
+        super(swerveDrive, shooter, conveyor, intake, hood, flap, visionModule, "Four- leave low left");
 
         addCommands(
-                followPathAndPickup("Four- leave low left"),
+                followPathAndPickup("Four- leave low left")
+        );
+
+        addCommands(
+                pickup(2)
+        );
+
+        addCommands(new ParallelRaceGroup(
                 followPath("Four- first shooting position"),
-                shootAndAdjust(3),
-                followPath("Four- go to terminal"),
-                followPathAndPickup("Four- pickup from terminal"),
-                followPath("Four- second shooting position"),
+                warmup(3400)
+        ));
+
+        addCommands(
                 shootAndAdjust(3)
         );
+
+        addCommands(
+                followPath("Four- go to terminal")
+        );
+
+        addCommands(
+                followPathAndPickup("Four- pickup from terminal")
+        );
+
+        addCommands(
+                pickup(2)
+        );
+
+        addCommands(
+                new ParallelRaceGroup(
+                        warmup(3400),
+                        followPath("Four- second shooting position")
+                ));
+
+        addCommands(
+                shootAndAdjust(3)
+        );
+
     }
 }
