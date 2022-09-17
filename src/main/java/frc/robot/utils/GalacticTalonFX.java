37bfc7bb98391d1349f1a35691b1804127a1754e
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-09-17 18:28:09 +0300

    Add units coefficient class member to `GalacticTalonFX`

--- src/main/java/frc/robot/utils/GalacticTalonFX.java
+++ src/main/java/frc/robot/utils/GalacticTalonFX.java
@@ -10,16 +10,19 @@ import edu.wpi.first.wpilibj.DriverStation;
  */
 public class GalacticTalonFX extends TalonFX {
 
+    private double unitsCoefficient;
+
     /**
      * Instantiates a Talon FX.
      * <p>
      * Reports configuration errors to the Driver Station.
      *
-     * @param deviceNumber  device number (ID) to assign to the Talon FX
-     * @param configuration configuration for the Talon FX
+     * @param deviceNumber     device number (ID) to assign to the Talon FX
+     * @param configuration    configuration for the Talon FX
+     * @param unitsCoefficient units ratio coefficient, e.g, to convert internal velocity units to m/s
      */
-    public GalacticTalonFX(int deviceNumber, TalonFXConfiguration configuration) {
-        this(deviceNumber, configuration, "");
+    public GalacticTalonFX(int deviceNumber, TalonFXConfiguration configuration, double unitsCoefficient) {
+        this(deviceNumber, configuration, unitsCoefficient, "");
     }
 
     /**
@@ -27,12 +30,14 @@ public class GalacticTalonFX extends TalonFX {
      * <p>
      * Reports configuration errors to the Driver Station.
      *
-     * @param deviceNumber  device number (ID) to assign to the Talon FX
-     * @param configuration configuration for the Talon FX
-     * @param canBus        Name of the CANbus; can be a SocketCAN interface (on Linux), or a CANivore device name or serial number
+     * @param deviceNumber     device number (ID) to assign to the Talon FX
+     * @param configuration    configuration for the Talon FX
+     * @param canBus           Name of the CANbus; can be a SocketCAN interface (on Linux), or a CANivore device name or serial number
+     * @param unitsCoefficient units ratio coefficient, e.g, to convert internal velocity units to m/s
      */
-    public GalacticTalonFX(int deviceNumber, TalonFXConfiguration configuration, String canBus) {
+    public GalacticTalonFX(int deviceNumber, TalonFXConfiguration configuration, double unitsCoefficient, String canBus) {
         super(deviceNumber, canBus);
+        this.unitsCoefficient = unitsCoefficient;
 
         ErrorCode errorCode = configAllSettings(configuration);
         if (errorCode != ErrorCode.OK) {
