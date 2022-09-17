a32a0bf92bf288827246d82e66e1353422b0fc34
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-09-17 18:28:09 +0300

    Create `GalacticTalonFX` class

---
+++ src/main/java/frc/robot/utils/GalacticTalonFX.java
@@ -0,0 +1,9 @@
+package frc.robot.utils;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+/**
+ * Class for custom functionality for the Talon FX, Falcon 500's controller.
+ */
+public class GalacticTalonFX extends TalonFX {
+}
